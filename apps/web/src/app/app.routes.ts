0984e943db38aa71327434260fdd675f6d6c22d4
JeremyKalhok <157445220+JeremyKalhok@users.noreply.github.com>
2025-11-24 18:28:37 -0500

    Create home page and product page

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Product } from '../product/product';
+import { Admin } from '../admin/admin';
+import { Home } from '../home/home';
+import { Login } from '../login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'product',
+        component: Product,
+    },
+    {
+        path: 'admin',
+        component: Admin,
+    },
+    {
+        path: 'login',
+        component: Login,
+    }
+];
---
+++ apps/web/src/product/product.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../navbar/navbar';
+
+@Component({
+  selector: 'app-product',
+  imports: [Navbar],
+  templateUrl: './product.html',
+  styleUrl: './product.scss',
+})
+export class Product {
+
+}
